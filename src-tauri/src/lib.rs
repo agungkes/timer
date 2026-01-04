93f1a6fc1ec98ca9856621d54145b4464c26522f
Agung Kurniawan <agungkes95@gmail.com>
2026-01-04 18:56:37 +0700

    refactor(lib): add http plugin and reorder imports
    - add tauri_plugin_http to application builder
    - reorder imports in lib.rs for consistency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use tauri::{Manager, PhysicalPosition, Emitter};
+use tauri::{Emitter, Manager, PhysicalPosition};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
             if args.len() > 1 {
-                let event_name = &args[1]; 
+                let event_name = &args[1];
                 app.emit("dslr-event", event_name).unwrap();
             }
         }))
@@ -20,14 +21,17 @@ pub fn run() {
 
             if let Some(monitor) = window.current_monitor().unwrap() {
                 let size = monitor.size();
-                println!("Resolusi monitor utama terdeteksi: {}x{}", size.width, size.height);
+                println!(
+                    "Resolusi monitor utama terdeteksi: {}x{}",
+                    size.width, size.height
+                );
 
                 let window_size = window.inner_size().unwrap(); // Ukuran jendela kita
                 let x = (size.width as i32 / 2) - (window_size.width as i32 / 2);
-                let y = 70; 
+                let y = 70;
 
                 window.set_position(PhysicalPosition::new(x, y)).unwrap();
-                
+
                 window.set_ignore_cursor_events(true).unwrap();
             }
 
