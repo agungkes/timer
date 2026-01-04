e2d278c6f68f1d6562b25e29b1d701af3dd8f993
Agung Kurniawan <agungkes95@gmail.com>
2026-01-04 18:43:45 +0700

    feat(timer): implement countdown logic and show window on session start
    - update package version to 0.1.1
    - remove status display and hardcoded countdown from html
    - initialize timer to 5 seconds on DOMContentLoaded
    - use event listener to show window and start timer on "session_start" event
    - implement timer display logic in main.js
    - hide window when timer reaches zero
    - change timer color to red when time is 10 seconds or less
    - set github actions to not draft releases

--- src/main.js
+++ src/main.js
@@ -1,42 +1,37 @@
 const { getCurrentWindow } = window.__TAURI__.window;
 const { listen } = window.__TAURI__.event;
 
-window.addEventListener("DOMContentLoaded", async () => {
+const startTimer = () => {
   let timeLeft = 5;
 
-  const statusEl = document.getElementById("status-display");
+  const timerInterval = setInterval(async () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    document.getElementById("time").textContent = `${String(minutes).padStart(
+      2,
+      "0"
+    )}:${String(seconds).padStart(2, "0")}`;
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      const appWindow = getCurrentWindow();
+      await appWindow.hide();
+    }
+    if (timeLeft <= 10) {
+      document.getElementById("timer-container").style.color = "#ff0000"; // Berubah merah saat kritis
+    }
+    timeLeft--;
+  }, 1000);
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+  startTimer();
 
   await listen("dslr-event", async (event) => {
     const dslrEvent = event.payload; // Ini berisi "session_start", "countdown_start", dll.
 
-    statusEl.innerText = `Event masuk: ${dslrEvent}`;
-
-    // Contoh: Jika countdown dslrBooth mulai, munculkan timer kita
-    // if (dslrEvent === "countdown_start") {
-    //   await getCurrentWindow().show();
-    //   startMyTimer(); // Fungsi timer yang sudah kamu buat
-    // }
+    if (dslrEvent === "session_start") {
+      await getCurrentWindow().show();
+      startTimer();
+    }
   });
-
-  // const timerInterval = setInterval(async () => {
-  //   const minutes = Math.floor(timeLeft / 60);
-  //   const seconds = timeLeft % 60;
-
-  //   document.getElementById("time").textContent = `${String(minutes).padStart(
-  //     2,
-  //     "0"
-  //   )}:${String(seconds).padStart(2, "0")}`;
-
-  //   if (timeLeft <= 0) {
-  //     clearInterval(timerInterval);
-  //     const appWindow = getCurrentWindow();
-  //     await appWindow.hide();
-  //   }
-
-  //   if (timeLeft <= 10) {
-  //     document.getElementById("timer-container").style.color = "#ff0000"; // Berubah merah saat kritis
-  //   }
-
-  //   timeLeft--;
-  // }, 1000);
 });
